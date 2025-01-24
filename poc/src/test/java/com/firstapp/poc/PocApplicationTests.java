a63d6a46afba72f3fa6d2c159bf198e1015c3b0f
vishnu-3001 <vishnusai3001@yahoo.com>
2025-01-23 22:09:32 -0500

    first springboot commit in 2.0 using vscode

---
+++ poc/src/test/java/com/firstapp/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstapp.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
